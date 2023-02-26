1b3397c9c7554f0646e6a04ac12897a56c12ef88
kumarch0601 <124835977+kumarch0601@users.noreply.github.com>
2023-02-26 11:33:44 -0600

    Create Numbers.java

---
+++ Numbers.java
@@ -0,0 +1,15 @@
+public class Numbers {
+    public static void main(String[] args) {
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+        String operator = args[2];
+        
+        if ("+".equals(operator)) {
+            System.out.println("Add: " + num1 + num2);
+        } else if ("-".equals(operator)) {
+            System.out.println("Substract: " + (num1 - num2));
+        } else {
+            System.out.println("Invalid operator");
+        }
+    }
+}
